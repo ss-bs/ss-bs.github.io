49051b48b3cc5f05140710a0aee4dc5138b3f937
ss-bs <97428516+ss-bs@users.noreply.github.com>
2022-01-14 21:38:27 +0530

    Add files via upload

---
+++ can't take my eyes off you 3/ss.js
@@ -0,0 +1,9 @@
+function display() {
+    document.getElementById('photoWrapper').style.display = "block"
+    document.getElementById("firstPage").style.display = "none"
+}
+var audio1 = document.getElementById("audioID");
+
+function playAudio() {
+    audio1.play();
+}
